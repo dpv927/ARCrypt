7ce7de0ecdc8b57622f4db57a1b00b3d3bcc1a82
dpv927 <dpv927@inlumine.ual.es>
2023-10-21 00:30:27 +0200

    Actualizar tamanio del buffer

--- openSSL/decryption.h
+++ openSSL/decryption.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DEC_BUFF_SIZE     2048 /* Tamano en bytes del buffer temporal de datos encriptados */
+#define DEC_BUFF_SIZE     8192 /* Tamano en bytes del buffer temporal de datos encriptados */
 #define DEC_CIPHER_SIZE   DEC_BUFF_SIZE /* Tamano en bytes del buffer de datos descifrados */
 #define DEC_ELEMENT_BYTES sizeof(unsigned char) /* Tamano en bytes por elemento a leer */
 
--- openSSL/encryption.h
+++ openSSL/encryption.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ENC_BUFF_SIZE     2048 /* Tamano en bytes del buffer temporal de encriptado */
+#define ENC_BUFF_SIZE     8192 /* Tamano en bytes del buffer temporal de encriptado */
 #define ENC_CIPHER_SIZE   ENC_BUFF_SIZE+EVP_MAX_BLOCK_LENGTH /* Tamano en bytes del buffer de datos cifrados */
 #define ENC_ELEMENT_BYTES sizeof(unsigned char) /* Tamano en bytes por elemento a leer */
 
