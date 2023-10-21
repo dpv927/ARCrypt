fb6802d0fcf2938c41258c740a30a83553bef625
dpv927 <dpv927@inlumine.ual.es>
2023-10-21 21:41:14 +0200

    Actualizar para probar

--- gtk/gtk-app.c
+++ gtk/gtk-app.c
@@ -1,9 +1,20 @@
 #include <gtk/gtk.h>
 
-int main(void) {
-    GtkApplication *app;
-    int status;
+int main(int argc, char *argv[]) {
+    GtkBuilder *builder;
+    GtkWidget *window;
     
+    /* Iniciar GTK */
+    gtk_init(&argc, &argv);
+    builder = gtk_builder_new();
+    
+    /* Obtener la ventana del archivo XML */
+    gtk_builder_add_from_file(builder, "./models/notebook2.glade", NULL);
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_widget_show_all(window);
+    gtk_main();
 
     return 0;
 }
+
