56e6465d7332759569f66bbbcdc4f003da9908f3
dpv927 <dpv927@inlumine.ual.es>
2023-10-21 21:42:51 +0200

    Modificar tabs

--- main.c
+++ main.c
@@ -27,12 +27,12 @@ int main(void) {
     getModeData(&data);
     
     switch (data.mode) {
-        case ENCRYPTION_MODE:
-            encryptFile(data.file_path, NULL);
-            break;
-        case DECRYPTION_MODE:
-            decryptFile(data.file_path, data.key_path, NULL);
-            break;
+      case ENCRYPTION_MODE:
+        encryptFile(data.file_path, NULL);
+        break;
+      case DECRYPTION_MODE:
+        decryptFile(data.file_path, data.key_path, NULL);
+        break;
     }   
     return EXIT_SUCCESS;
 }
@@ -43,21 +43,21 @@ void getAppMode(OperationData* d) {
     int mode;
     
     printf("Elige el modo de operacion [0/1]: \n" \
-           "0] Encriptar archivo\n1] Desencriptar archivo.\n>> ");
+        "0] Encriptar archivo\n1] Desencriptar archivo.\n>> ");
     scanf("%s", input);
     strtol(input, &endptr, 10);
 
     if(*endptr == '\0') {
-        mode = atoi(input);
+      mode = atoi(input);
         
-        if(mode != ENCRYPTION_MODE && mode != DECRYPTION_MODE) {
-            printf("'%s': No es un modo valido!\n", input);
-            exit(EXIT_FAILURE);
-        }   
-        d->mode = (uint8_t) mode;
-    } else {
+      if(mode != ENCRYPTION_MODE && mode != DECRYPTION_MODE) {
         printf("'%s': No es un modo valido!\n", input);
         exit(EXIT_FAILURE);
+      }   
+      d->mode = (uint8_t) mode;
+    } else {
+      printf("'%s': No es un modo valido!\n", input);
+      exit(EXIT_FAILURE);
     }
 }
 
@@ -65,22 +65,22 @@ void getModeData(OperationData* d) {
     char input[2048];
 
     switch (d->mode) {
-        case ENCRYPTION_MODE:    
-            printf("\nIntroduce la ruta del archivo a encriptar:\n"\
-                   ">> ");
-            scanf("%s", input);
-            strcpy(d->file_path, input);
-            break;
+      case ENCRYPTION_MODE:    
+        printf("\nIntroduce la ruta del archivo a encriptar:\n"\
+            ">> ");
+        scanf("%s", input);
+        strcpy(d->file_path, input);
+        break;
 
-        case DECRYPTION_MODE:
-            printf("\nIntroduce la ruta del archivo a desencriptar:\n"\
-                   ">> ");
-            scanf("%s", input);
-            strcpy(d->file_path, input);
-            printf("\nIntroduce la ruta de la clave:\n"\
-                   ">> ");
-            scanf("%s", input);
-            strcpy(d->key_path, input);
-            break;
+      case DECRYPTION_MODE:
+        printf("\nIntroduce la ruta del archivo a desencriptar:\n"\
+          ">> ");
+        scanf("%s", input);
+        strcpy(d->file_path, input);
+        printf("\nIntroduce la ruta de la clave:\n"\
+          ">> ");
+        scanf("%s", input);
+        strcpy(d->key_path, input);
+        break;
     }
 }
