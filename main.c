5c67a3d682a45a5a31a492b877b541b85fe46728
dpv927 <dpv927@inlumine.ual.es>
2023-10-28 16:44:22 +0200

    Reformatear

--- main.c
+++ main.c
@@ -12,9 +12,9 @@
 
 /* Datos necesarios */
 typedef struct {
-    uint8_t mode;
-    char file_path[FILE_PATH_BYTES];
-    char key_path[FILE_PATH_BYTES];
+  uint8_t mode;
+  char file_path[FILE_PATH_BYTES];
+  char key_path[FILE_PATH_BYTES];
 } OperationData;
 
 /* Funciones de ontencion de datos */
@@ -22,65 +22,65 @@ void getAppMode(OperationData* d);
 void getModeData(OperationData* d);
 
 int main(void) {
-    OperationData data;
-    getAppMode(&data);
-    getModeData(&data);
-    
-    switch (data.mode) {
-      case ENCRYPTION_MODE:
-        encryptFile(data.file_path, NULL);
-        break;
-      case DECRYPTION_MODE:
-        decryptFile(data.file_path, data.key_path, NULL);
-        break;
-    }   
-    return EXIT_SUCCESS;
+  OperationData data;
+  getAppMode(&data);
+  getModeData(&data);
+   
+  switch (data.mode) {
+    case ENCRYPTION_MODE:
+      encryptFile(data.file_path, NULL);
+      break;
+    case DECRYPTION_MODE:
+      decryptFile(data.file_path, data.key_path, NULL);
+      break;
+  }   
+  return EXIT_SUCCESS;
 }
 
 void getAppMode(OperationData* d) {
-    char input[100];
-    char* endptr;
-    int mode;
+  char input[100];
+  char* endptr;
+  int mode;
     
-    printf("Elige el modo de operacion [0/1]: \n" \
-        "0] Encriptar archivo\n1] Desencriptar archivo.\n>> ");
-    scanf("%s", input);
-    strtol(input, &endptr, 10);
+  printf("Elige el modo de operacion [0/1]: \n" \
+    "0] Encriptar archivo\n1] Desencriptar archivo.\n>> ");
+  scanf("%s", input);
+  strtol(input, &endptr, 10);
 
-    if(*endptr == '\0') {
-      mode = atoi(input);
+  if(*endptr == '\0') {
+    mode = atoi(input);
         
-      if(mode != ENCRYPTION_MODE && mode != DECRYPTION_MODE) {
-        printf("'%s': No es un modo valido!\n", input);
-        exit(EXIT_FAILURE);
-      }   
-      d->mode = (uint8_t) mode;
-    } else {
+    if(mode != ENCRYPTION_MODE && mode != DECRYPTION_MODE) {
       printf("'%s': No es un modo valido!\n", input);
       exit(EXIT_FAILURE);
-    }
+    }   
+    d->mode = (uint8_t) mode;
+  } else {
+    printf("'%s': No es un modo valido!\n", input);
+    exit(EXIT_FAILURE);
+  }
 }
 
 void getModeData(OperationData* d) {
-    char input[2048];
+  char input[2048];
 
-    switch (d->mode) {
-      case ENCRYPTION_MODE:    
-        printf("\nIntroduce la ruta del archivo a encriptar:\n"\
-            ">> ");
-        scanf("%s", input);
-        strcpy(d->file_path, input);
-        break;
+  switch (d->mode) {
+    case ENCRYPTION_MODE:    
+      printf("\nIntroduce la ruta del archivo a encriptar:\n"\
+        ">> ");
+      scanf("%s", input);
+      strcpy(d->file_path, input);
+      break;
 
-      case DECRYPTION_MODE:
-        printf("\nIntroduce la ruta del archivo a desencriptar:\n"\
-          ">> ");
-        scanf("%s", input);
-        strcpy(d->file_path, input);
-        printf("\nIntroduce la ruta de la clave:\n"\
-          ">> ");
-        scanf("%s", input);
-        strcpy(d->key_path, input);
-        break;
+    case DECRYPTION_MODE:
+      printf("\nIntroduce la ruta del archivo a desencriptar:\n"\
+        ">> ");
+      scanf("%s", input);
+      strcpy(d->file_path, input);
+      printf("\nIntroduce la ruta de la clave:\n"\
+        ">> ");
+      scanf("%s", input);
+      strcpy(d->key_path, input);
+      break;
     }
 }
