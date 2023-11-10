c4054d9c7dad79c3f3c54e6fc789491310d2ed61
dpv927 <dpv927@inlumine.ual.es>
2023-11-10 02:27:08 +0100

    embellecer modo terminal

--- term_mode.c
+++ term_mode.c
@@ -2,11 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 #include "term_mode.h"
+#include "utils/messages.h"
 #include "openSSL/encryption.h"
 #include "openSSL/decryption.h"
 
 void init_term(void) {
   OperationData data;
+
+  /* Mostar el logo */
+  system("clear");
+  FILE* ptr = fopen("extra/logo.txt", "r");
+  char buff[100];
+
+  while (fgets(buff, sizeof(buff), ptr) != NULL)
+    printf("%s", buff);
+  printf("\n");
+  fclose(ptr);
+
+  /* Obtener el modo */
   getAppMode(&data);
   getModeData(&data);
    
@@ -24,22 +37,23 @@ void getAppMode(OperationData* d) {
   char input[100];
   char* endptr;
   int mode;
-    
-  printf("Elige el modo de operacion [0/1]: \n" \
-    "0] Encriptar archivo\n1] Desencriptar archivo.\n>> ");
-  scanf("%s", input);
+  
+  print_title("Modos")
+  print_option(0, "Encriptar archivo")
+  print_option(1, "Desencriptar archivo")
+  user_input("Elige un modo valido", "%s", input);
   strtol(input, &endptr, 10);
 
   if(*endptr == '\0') {
     mode = atoi(input);
         
     if(mode != ENCRYPTION_MODE && mode != DECRYPTION_MODE) {
-      printf("'%s': No es un modo valido!\n", input);
+      printf("\nError: '%s': No es un modo valido!\n", input);
       exit(EXIT_FAILURE);
     }   
     d->mode = (uint8_t) mode;
   } else {
-    printf("'%s': No es un modo valido!\n", input);
+    printf("\nError: '%s': No es un modo valido!\n", input);
     exit(EXIT_FAILURE);
   }
 }
@@ -49,23 +63,24 @@ void getModeData(OperationData* d) {
 
   switch (d->mode) {
     case ENCRYPTION_MODE:    
-      printf("\nIntroduce la ruta del archivo a encriptar:\n"\
-        ">> ");
-      scanf("%s", input);
-      printf("\n");
+      next_line()
+      print_title("Encriptacion")
+      user_input("Ruta del archivo a encriptar", "%s", input)
+      next_line()
+
       strcpy(d->file_path, input);
+      exit(0);
       break;
 
     case DECRYPTION_MODE:
-      printf("\nIntroduce la ruta del archivo a desencriptar:\n"\
-        ">> ");
-      scanf("%s", input);
+      next_line()
+      print_title("Desencriptacion")
+      user_input("Ruta del archivo a desencriptar", "%s", input)
       strcpy(d->file_path, input);
-      printf("\nIntroduce la ruta de la clave:\n"\
-        ">> ");
-      scanf("%s", input);
-      printf("\n");
+
+      user_input("Ruta de la clave", "%s", input)
       strcpy(d->key_path, input);
+      next_line()
       break;
     }
 }
--- utils/messages.h
+++ utils/messages.h
@@ -1,10 +1,34 @@
 #pragma once
 
+/* Imprimir un mensaje de informacion */
 #define p_info(message)\
   printf("\033[1;32mInfo:\033[0m %s\n", message);
 
+/* Imprimir un mensaje de informacion doble */
 #define p_infoString(message, string)\
     printf("\033[1;32mInfo:\033[0m %s %s\n", message, string);
 
+/* Imprimir un mensaje de error */
 #define p_error(message)\
   printf("\033[1;31mError:\033[0m %s\n", message);
+
+/* Imprimir un mensaje de error doble */
+#define p_error(message)\
+  printf("\033[1;31mError:\033[0m %s\n", message);
+
+#define next_line()\
+  printf("\n");
+
+/* Obtener un input del usuario */
+#define user_input(about, format, var)\
+  printf("\033[0;31m[\033[0;33m%s\033[0;31m]->\033[0m ", about);\
+  scanf(format, var);
+  
+/* Imprimir el titulo de una seccion */
+#define print_title(text)\
+  printf("\033[0;31m[%s\033[0;31m]\n", text);
+
+/* Imprimir una opcion de un menu */
+#define print_option(index, text)\
+  printf("\033[0;31m[\033[0;33m%d\033[0;31m]\033[0m %s\n",\
+    index, text);
